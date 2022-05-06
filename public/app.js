63c999bb537dc629d1c8cbd9e3acbe650f58b672
deankp <dperez0689@gmail.com>
2022-05-05 20:01:38 -0500

    finished

--- public/app.js
+++ public/app.js
@@ -4,11 +4,8 @@ const myMap = L.map('map', {
     zoom: 12,
 });
 
-
-// add openstreetmap tiles
-
 // Add OpenStreetMap tiles:
-L.tileLayer('https://.tile.openstreetmap.org///.png', {
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: '15',
 }).addTo(myMap)
@@ -25,20 +22,28 @@ const latlngs = [[48.863368120198004, 2.3509079846928516],
 [48.86933262048345, 2.3542531602919805],
 [48.87199261164275, 2.3400569901592183],
 [48.86993336274516, 2.3280142476578813], 
-[48.86834104280146, 2.330308418109664]]
+[48.86834104280146, 2.330308418109664]];
 
-const polygon = L.polygon(latlngs, {color: 'blue'}).addTo(map);
+const polygon = L.polygon(latlngs, {
+    color: 'blue', 
+    fillOpacity: 0.0
+}).addTo(myMap)
 
 
 // create red pin marker
+const redPin = L.icon({
+    iconUrl: './assets/red-pin.png',
+    iconSize: [38,38],
+    iconAnchor: [19, 38]
+})
 
 // Metro station markers:
-const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('RÃ©aumur-SÃ©bastopol')
-const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
-const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
-const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
-const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
-const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
+const rS = L.marker([48.866200610611926, 2.352236247419453],{icon: redPin}).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196],{icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739],{icon: redPin}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167],{icon: redPin}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543],{icon: redPin}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714],{icon: redPin}).bindPopup('Grands Boulevards')
 
 const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
